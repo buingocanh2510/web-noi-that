75124e2ceb1ec95dda85428a676acca91fc45098
buingocanh2510 <anhkute2b@gmail.com>
2021-10-31 23:42:32 +0700

    Init project

---
+++ src/main/java/com/webnoithat/webnoithat/model/Product.java
@@ -0,0 +1,40 @@
+package com.webnoithat.webnoithat.model;
+
+public class Product {
+    private String name;
+    private double price;
+    private String description;
+    private String imageUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
